305b152e9181c4f2f8ee3fd2585a8f96535fa0ae
Kaiden Simon <69560455+485585r@users.noreply.github.com>
2025-01-13 13:29:35 -0800

    Update CONFIG.py

--- TENSORFLOWMODELGEN/CONFIG.py
+++ TENSORFLOWMODELGEN/CONFIG.py
@@ -1,4 +1,3 @@
-DATA_DIR = r"D:\fastaimodelmaker\fungi"
-SAMPLE_IM_PATH = r"D:\fastaimodelmaker\Data\glioma_tumor\G_1_HF_.jpg"
+DATA_DIR = r"D:/TENSORFLOWMODELGEN/fungi"
+SAMPLE_IM_PATH = r"D:/TENSORFLOWMODELGEN/fungi/H1/H1_1a_1.jpg"
 DOCUMENTATION = ["https://www.tensorflow.org/tutorials/images/classification"]
-
